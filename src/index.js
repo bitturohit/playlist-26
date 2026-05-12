9aad8d768ff49bf3ba2ce9fb54ef244570306663
bitturohit <rohit97750@gmail.com>
2026-05-12 17:28:58 +0530

    initial progress

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import "bulma/css/bulma.css";
 import { createRoot } from "react-dom/client";
 
 import App from "./App.js";
+import "./store";
 
 createRoot(document.getElementById("root")).render(<App />);
