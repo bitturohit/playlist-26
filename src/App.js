62804c8d7a524980f071b65545f7dc48ad594d31
bitturohit <rohit97750@gmail.com>
2026-06-25 10:24:49 +0530

    geeting an action creator type

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./styles.css";
 
+import { useDispatch } from "react-redux";
+
+import { reset } from "./store";
 import MoviePlaylist from "./components/MoviePlaylist";
 import SongPlaylist from "./components/SongPlaylist";
 
 export default function App() {
+   const dispatch = useDispatch();
+
    const handleResetClick = () => {
-      //
+      dispatch(reset());
    };
 
    return (
